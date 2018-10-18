e75ac2521411bb82200e9bf611c47935219aa8e9
FilUnderscore <filjerkovic@gmail.com>
2018-10-18 15:36:27 +1300

    Update assemblyinfo

--- Source/PersistentRimWorlds/Properties/AssemblyInfo.cs
+++ Source/PersistentRimWorlds/Properties/AssemblyInfo.cs
@@ -1,25 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PersistentRimWorlds")]
 [assembly: AssemblyDescription("Turns a rimworld into a persistent rimworld, explore fallen colonies where pirates may lurk and stop by your predecessors.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PersistentWorlds")]
 [assembly: AssemblyCopyright("Copyright © FilUnderscore 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("58FE1008-FE3A-44FC-A71A-4E9315B76A94")]
 
 // Version information for an assembly consists of the following four values:
 //
