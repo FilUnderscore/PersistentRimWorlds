770a4f1d96a0a1864ce3aeec14bcd210f593f03d
FilUnderscore <filjerkovic@gmail.com>
2018-10-20 02:43:43 +1300

    Rename from Debug to DebugUtils.

---
+++ Source/PersistentRimWorlds/DebugUtils/DebugExtensions.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersistentWorlds.Debug
+{
+    public static class DebugExtensions
+    {
+        public static string ToDebugString<TK, TV>(this Dictionary<TK, TV> dict)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("{0} (keytype={1}, valuetype={2})", nameof(dict), nameof(TK), nameof(TV));
+            sb.Append("{");
+            
+            for (var i = 0; i < dict.Count; i++)
+            {
+                var pair = dict.ElementAt(i);
+                
+                var key = pair.Key;
+                var value = pair.Value;
+
+                sb.AppendFormat("[key={0}, value={1}]", key.ToString(), value.ToString());
+                
+                if (i != dict.Count - 1)
+                {
+                    // Comma.
+                    sb.Append(", ");
+                }
+            }
+
+            sb.Append("}");
+            
+            return sb.ToString();
+        }
+
+        public static string ToDebugString<TE>(this List<TE> list)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("{0} (type={1})", nameof(list), nameof(TE));
+            sb.Append("{");
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                var element = list[i];
+
+                sb.AppendFormat("[index={0}, element={1}]", i, element.ToString());
+
+                if (i != list.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+            
+            sb.Append("}");
+            
+            return sb.ToString();
+        }
+
+        public static string ToDebugString<TE>(this HashSet<TE> set)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("{0} (type={1})", nameof(set), nameof(TE));
+            sb.Append("{");
+
+            var i = 0;
+            
+            foreach (var element in set)
+            {
+                sb.AppendFormat("[element={0}]", element.ToString());
+
+                if (i != set.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+                
+                i++;
+            }
+            
+            sb.Append("}");
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ Source/PersistentRimWorlds/DebugUtils/FileLog.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Reflection;
+
+namespace PersistentWorlds.Debug
+{
+    public static class FileLog
+    {
+        #region Fields
+        private static StreamWriter LogWriter;
+        #endregion
+        
+        #region Methods
+        public static void StartLoggingToFile(string filePath)
+        {
+            LogWriter = new StreamWriter(filePath, false);
+            Log("Persistent RimWorlds Debug Log (AssemblyVersion=" + Assembly.GetExecutingAssembly().GetName().Version + ")");
+        }
+
+        public static void Log(string text)
+        {
+            LogWriter.Write(text + "\n");
+            LogWriter.Flush();
+        }
+
+        public static void Close()
+        {
+            LogWriter.Close();
+        }
+        #endregion
+    }
+}
