751bdae1dd37acc7fa4f32a969b13a7eee7e967b
FilUnderscore <filjerkovic@gmail.com>
2018-10-31 19:00:47 +1300

    Patch mainmenudrawer more.

--- Source/PersistentRimWorlds/Patches/MainMenuDrawer_DoMainMenuControls_Patch.cs
+++ Source/PersistentRimWorlds/Patches/MainMenuDrawer_DoMainMenuControls_Patch.cs
@@ -7,6 +7,7 @@ using Harmony;
 using PersistentWorlds.SaveAndLoad;
 using RimWorld;
 using Verse;
+using FileLog = PersistentWorlds.Utils.FileLog;
 
 namespace PersistentWorlds.Patches
 {
@@ -28,6 +29,14 @@ namespace PersistentWorlds.Patches
 
         private static readonly MethodInfo AnyWorldsMethod =
             AccessTools.Method(typeof(SaveFileUtils), "AnyWorlds");
+
+        private static readonly MethodInfo GetGameMethod = AccessTools.Method(typeof(Current), "get_Game");
+
+        private static readonly MethodInfo GetInstanceMethod =
+            AccessTools.Method(typeof(PersistentWorldManager), "GetInstance");
+
+        private static readonly MethodInfo PersistentWorldNotNullMethod =
+            AccessTools.Method(typeof(PersistentWorldManager), "PersistentWorldNotNull");
         #endregion
         
         #region Methods
@@ -35,41 +44,73 @@ namespace PersistentWorlds.Patches
         {
             var codes = new List<CodeInstruction>(instr);
 
-            var jumpLabel = ilGen.DefineLabel();
+            var mainJumpLabel = ilGen.DefineLabel();
+            
+            var saveSkipLabel = ilGen.DefineLabel();
+            var saveJumpLabel = ilGen.DefineLabel();
             
             for (var i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode != OpCodes.Call) continue;
-                if (codes[i + 4].opcode != OpCodes.Ldstr) continue;
-                if ((codes[i + 4].operand as string) != "ReviewScenario") continue;
-
-                codes[i].labels.Add(jumpLabel);
-                
-                var insertInstr = new List<CodeInstruction>
+                if (codes[i].opcode == OpCodes.Call && codes[i].operand == GetGameMethod &&
+                    codes[i + 5].opcode == OpCodes.Ldstr && codes[i + 5].operand as string == "Save")
                 {
-                    new CodeInstruction(OpCodes.Call, AnyWorldsMethod),
-                    new CodeInstruction(OpCodes.Brfalse_S, jumpLabel),
+                    Log.Message("Patched");
                     
-                    new CodeInstruction(OpCodes.Ldstr, "FilUnderscore.PersistentRimWorlds"),
-                    new CodeInstruction(OpCodes.Call,TranslateMethod),
-                    new CodeInstruction(OpCodes.Ldsfld, MainMenuButtonDelegate),
-                    new CodeInstruction(OpCodes.Castclass, typeof(Action)),
-                    new CodeInstruction(OpCodes.Ldnull),
-                    new CodeInstruction(OpCodes.Newobj, ListableOptionConstructor),
-                    new CodeInstruction(OpCodes.Stloc_3),
-                    new CodeInstruction(OpCodes.Ldloc_2),
-                    new CodeInstruction(OpCodes.Ldloc_3),
-                    new CodeInstruction(OpCodes.Callvirt, AddMethod)
-                };
+                    codes[i + 4].labels.Add(saveJumpLabel);
+                    codes[i + 17].labels.Add(saveSkipLabel);
+                    
+                    var insertInstr = new List<CodeInstruction>
+                    {
+                        new CodeInstruction(OpCodes.Call, GetInstanceMethod),
+                        new CodeInstruction(OpCodes.Callvirt, PersistentWorldNotNullMethod),
+                        new CodeInstruction(OpCodes.Brfalse_S, saveJumpLabel),
+                        
+                        new CodeInstruction(OpCodes.Ldloc_2),
+                        new CodeInstruction(OpCodes.Ldstr, "SaveWorld"),
+                        new CodeInstruction(OpCodes.Call, TranslateMethod),
+                        new CodeInstruction(OpCodes.Ldsfld, MainMenuButtonDelegate),
+                        new CodeInstruction(OpCodes.Castclass, typeof(Action)),
+                        new CodeInstruction(OpCodes.Ldnull),
+                        new CodeInstruction(OpCodes.Newobj, ListableOptionConstructor),
+                        new CodeInstruction(OpCodes.Callvirt, AddMethod),
+                        new CodeInstruction(OpCodes.Br_S, saveSkipLabel)
+                    };
+                    
+                    codes.InsertRange(i + 4, insertInstr);
+                }
+                else if (codes[i].opcode == OpCodes.Call && codes[i + 4].opcode == OpCodes.Ldstr &&
+                    codes[i + 4].operand as string == "ReviewScenario")
+                {
+                    codes[i].labels.Add(mainJumpLabel);
 
-                codes[i].labels.DoIf(label => label != jumpLabel, insertInstr[0].labels.Add);
-                codes[i].labels.RemoveAll(label => label != jumpLabel);
-                
-                codes.InsertRange(i, insertInstr);
-                
-                break;
+                    var insertInstr = new List<CodeInstruction>
+                    {
+                        new CodeInstruction(OpCodes.Call, AnyWorldsMethod),
+                        new CodeInstruction(OpCodes.Brfalse_S, mainJumpLabel),
+
+                        new CodeInstruction(OpCodes.Ldstr, "FilUnderscore.PersistentRimWorlds"),
+                        new CodeInstruction(OpCodes.Call, TranslateMethod),
+                        new CodeInstruction(OpCodes.Ldsfld, MainMenuButtonDelegate),
+                        new CodeInstruction(OpCodes.Castclass, typeof(Action)),
+                        new CodeInstruction(OpCodes.Ldnull),
+                        new CodeInstruction(OpCodes.Newobj, ListableOptionConstructor),
+                        new CodeInstruction(OpCodes.Stloc_3),
+                        new CodeInstruction(OpCodes.Ldloc_2),
+                        new CodeInstruction(OpCodes.Ldloc_3),
+                        new CodeInstruction(OpCodes.Callvirt, AddMethod)
+                    };
+
+                    codes[i].labels.DoIf(label => label != mainJumpLabel, insertInstr[0].labels.Add);
+                    codes[i].labels.RemoveAll(label => label != mainJumpLabel);
+
+                    codes.InsertRange(i, insertInstr);
+
+                    break;
+                }
             }
             
+            codes.Do(code => FileLog.Log(code.ToString()));
+
             return codes.AsEnumerable();
         }
         #endregion
